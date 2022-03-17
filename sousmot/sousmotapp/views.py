013bea72f14dae35d1a9fbe48854baf34746c66d
Alexia Montavon <alexia@montavon.xyz>
2022-03-17 01:01:19 +0100

    feat(#12): Add homepage basic template

--- sousmot/sousmotapp/views.py
+++ sousmot/sousmotapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'sousmotapp/index.html', context)
