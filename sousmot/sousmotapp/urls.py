2d24f3dc0299e046832b1c0f7bbfb86cc2fe517b
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-03-23 09:45:53 +0100

    feat(#13): signup form

--- sousmot/sousmotapp/urls.py
+++ sousmot/sousmotapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+from .views import SignUpView
 
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),  # new
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
