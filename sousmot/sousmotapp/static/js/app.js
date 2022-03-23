8807e58056f9481176dda3b7340c75524074b60f
Alexia Montavon <alexia@montavon.xyz>
2022-03-23 12:07:10 +0100

    chore(#12): Add JS file

---
+++ sousmot/sousmotapp/static/js/app.js
@@ -0,0 +1,17 @@
+
+
+// Create Game form : Toggle mode button
+let gamemodeList = [];
+const gameModeInput = document.getElementById("gamemode-input");
+
+document.querySelectorAll(".field.gamemode .button").forEach( el => {
+    el.addEventListener("click", ev => {
+        gamemodeList.forEach( i => i.classList.remove("is-selected", "is-crimson"));
+        el.classList.add("is-selected", "is-crimson");
+        gameModeInput.value = el.dataset.gamemode;
+
+    });
+    gamemodeList.push(el);
+})
+
+// Slider management
