a5bbf05973205a34be281fff21d33a847eea4fa6
Corentin Faivre <corentin.faivre@he-arc.ch>
2022-03-23 09:08:08 +0100

    feat(#1): create databse in admin console

--- sousmot/sousmotapp/admin.py
+++ sousmot/sousmotapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, Dictionary, Word, Mode, Game, Games_per_users, History_solo
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Dictionary)
+admin.site.register(Word)
+admin.site.register(Mode)
+admin.site.register(Game)
+admin.site.register(Games_per_users)
+admin.site.register(History_solo)
