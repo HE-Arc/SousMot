5dbf1107c40cc895703e76d2791a1e07f9566f1f
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-03-23 09:02:50 +0100

    feat:(#13): simple login page

--- sousmot/sousmotapp/urls.py
+++ sousmot/sousmotapp/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+]
