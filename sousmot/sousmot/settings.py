a0903bdf38ddd7b399851fc795d8aec9bedf4049
Alexia Montavon <alexia@montavon.xyz>
2022-03-24 16:37:49 +0100

    chore(#23): Add environ to the settings file

--- sousmot/sousmot/settings.py
+++ sousmot/sousmot/settings.py
@@ -10,21 +10,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os.path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Environment variable
+env = environ.Env(
+    SOUSMOT_DEBUG_MODE=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@b8-slt)-=+63jwfg5k0x8vzo-h=9w7t*g18)==qdqed=8pvhe'
+SECRET_KEY = env('SOUSMOT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('SOUSMOT_DEBUG_MODE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('SOUSMOT_ALLOWED_HOSTS')
 
 
 # Application definition
