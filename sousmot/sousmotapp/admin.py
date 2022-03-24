81faa5e90a27bb2b580a817cc9ed2dd7f7090bbb
Corentin Faivre <corentin.faivre@he-arc.ch>
2022-03-24 22:08:38 +0100

    fix(#1): rename table database

--- sousmot/sousmotapp/admin.py
+++ sousmot/sousmotapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Dictionary, Word, Mode, Game, GamesPerUsers, HistorySolo
+from .models import User, Dictionary, Word, Mode, Game, GamesPerUser, HistorySolo
 # Register your models here.
 
 admin.site.register(User)
@@ -8,5 +8,5 @@ admin.site.register(Dictionary)
 admin.site.register(Word)
 admin.site.register(Mode)
 admin.site.register(Game)
-admin.site.register(GamesPerUsers)
+admin.site.register(GamesPerUser)
 admin.site.register(HistorySolo)
