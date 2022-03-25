77398f5e55eb5c591cf2dbe65434ad8d9737a548
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-03-25 15:20:48 +0100

    feat(#13): already logged user redirection

--- sousmot/sousmotapp/views.py
+++ sousmot/sousmotapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+
+
 # Create your views here.
 
 def index(request):
@@ -13,3 +15,8 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('/')
+        return super().dispatch(*args, **kwargs)
