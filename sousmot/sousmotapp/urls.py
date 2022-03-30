f2b272979dd8024e3d7a619bc22de6271eea2989
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-03-30 08:35:09 +0200

    feat(#14): create game page

--- sousmot/sousmotapp/urls.py
+++ sousmot/sousmotapp/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('game/', views.game, name='index')
 ]
--- sousmot/sousmotapp/views.py
+++ sousmot/sousmotapp/views.py
@@ -5,3 +5,8 @@ from django.views import View
 def index(request):
     context = {}
     return render(request, 'sousmotapp/index.html', context)
+
+
+def game(request):
+    context = {}
+    return render(request, 'sousmotapp/game.html', context)
