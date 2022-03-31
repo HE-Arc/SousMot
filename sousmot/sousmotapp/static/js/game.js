a177a45e17f0352c64bcba2add7b1d9a08081600
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-03-31 15:08:13 +0200

    chore(#14): clean code

--- sousmot/sousmotapp/static/js/game.js
+++ sousmot/sousmotapp/static/js/game.js
@@ -1,12 +1,13 @@
-var userTry = 0;
-var userPosition = 1;
-var word = wordFirstLetter.concat('.'.repeat(wordLength - 1));
+let userTry = 0;
+let userPosition = 1;
+let word = wordFirstLetter.concat('.'.repeat(wordLength - 1));
 writeWord();
 
 document.addEventListener('keydown', (event) => {
-    var letter = event.key;
-    var code = event.code;
+    let letter = event.key;
     addLetterToWord(letter);
+
+    //var code = event.code;
     //alert(`Key pressed ${letter} \r\n Key code value: ${code}`);
 }, false);
 
@@ -28,9 +29,9 @@ function addLetterToWord(letter) {
 }
 
 function writeWord() {
-    var myTable = document.getElementById('table');
-    var rows = myTable.rows;
-    var inputRow = rows[userTry];
+    let myTable = document.getElementById('table');
+    let rows = myTable.rows;
+    let inputRow = rows[userTry];
     for (let i = 0; i < wordLength; i++) {
         inputRow.cells[i].innerText = word[i];
     }
