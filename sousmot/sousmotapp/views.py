0977f788fb57dad20d842cc19e3ce8a2419723af
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-04-06 23:10:11 +0200

    feat(#16): add rule page

--- sousmot/sousmotapp/views.py
+++ sousmot/sousmotapp/views.py
@@ -11,6 +11,11 @@ def index(request):
     return render(request, 'sousmotapp/index.html', context)
 
 
+def rules(request):
+    context = {}
+    return render(request, 'sousmotapp/rules.html', context)
+
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
