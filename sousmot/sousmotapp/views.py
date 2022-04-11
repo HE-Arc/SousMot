5beed1528a545096762dffe74b036e1cbd9d9747
Massimo Ferro <massimo.ferro@he-arc.ch>
2022-04-11 23:52:33 +0200

    feat(#8): attach game view to backend

--- sousmot/sousmotapp/views.py
+++ sousmot/sousmotapp/views.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 import time
 from .models import Game
 from django.shortcuts import render, redirect
@@ -8,42 +9,55 @@ from django.utils.decorators import method_decorator
 from django.views import generic, View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
-from django.http import JsonResponse
-from .models import Game, Mode, Dictionary
+from django.http import JsonResponse, Http404
+from .models import Game, Mode, Dictionary, Word
+from django.db.models.functions import Length
 
 
 def index(request):
-    context = { 
-        'is_guest' : request.user.is_authenticated,
+    context = {
+        'is_guest': request.user.is_authenticated,
     }
     return render(request, 'sousmotapp/index.html', context)
+
 
 def rules(request):
     context = {}
     return render(request, 'sousmotapp/rules.html', context)
-  
 
-class GameView(generic.TemplateView):
+
+class GameView(generic.View):
     template_name = 'sousmotapp/game.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def post(self, request):
+        game_mode = request.POST.get('gamemode')
+        game_duration = request.POST.get('game_duration')
+        word_length = int(request.POST.get('word_length'))
+        dictionary_pk = request.POST.get('dictionary')
 
-        context['mode'] = "TIME ATTACK"
-        context['rows'] = range(6)
+        if game_mode == 'time-attack':
+            seconds = int(game_duration.split(":")[0]) * 60 + int(game_duration.split(":")[1])
+            end_time = time.time() + seconds
+            number_words = int(seconds / 5)
+            words = Word.objects.annotate(word_len=Length('word')).filter(dictionary=dictionary_pk,
+                                                                          word_len__exact=word_length)
+            generated_words = random.sample(list(words), number_words)
+            upper_generated_words = list(map(lambda x: x.word.upper(), generated_words))
 
-        end_time = time.time() + 610  # TODO replace by game duration
-        context['end_time'] = end_time
+            context = {
+                'mode': "TIME ATTACK",
+                'rows': range(6),
+                'end_time': end_time,
+                'word_length': range(word_length),
+                'word_length_js': word_length,
+                'word_first_letter': upper_generated_words[0][0]
+            }
 
-        word_length = 5  # TODO replace by word length
-        context['word_length'] = range(word_length)
-        context['word_length_js'] = word_length
+            return render(request, 'sousmotapp/game.html', context)
+        else:
+            raise Http404("Not implemented baby !")
 
-        context['word_first_letter'] = "P"  # TODO replace by first letter of word
 
-        return context
-
-      
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
@@ -68,6 +82,8 @@ class GameLobbyView(TemplateView):
             context["is_guest"] = True
         else:
             context["username"] = self.request.user.username
+
+        context["dictionaries"] = Dictionary.objects.all()
 
         # TODO: Uncomment
         # if kwargs["slug"] in self.request.session["creator"]:
