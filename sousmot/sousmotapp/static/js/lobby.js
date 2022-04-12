07766e48112109178dda17e8b911c8f55f11db5c
Corentin Faivre <corentin.faivre@he-arc.ch>
2022-04-13 00:23:12 +0200

    feat: type of user in websocket

--- sousmot/sousmotapp/static/js/lobby.js
+++ sousmot/sousmotapp/static/js/lobby.js
@@ -6,23 +6,37 @@ const chatSocket = new WebSocket(url);
 
 function createHtmlUser(username, is_guest)
 {
-    return `
-    <article class="media">
-        <figure class="media-left">
-            <i class="fa fa-user fa-2x" aria-hidden="true"></i>
-        </figure>
-        <div class="media-content">
-            <div class="content">
-                <p><strong>${username}</strong> <span class="tag is-link">Guest</span></p>
+    if(is_guest)
+        return `
+        <article class="media">
+            <figure class="media-left">
+                <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p><strong>${username}</strong> <span class="tag is-link">Guest</span></p>
+                </div>
             </div>
-        </div>
-    </article>
-    `;
+        </article>
+        `;
+    else
+        return `
+        <article class="media">
+            <figure class="media-left">
+                <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p><strong>${username}</strong> <span class="tag is-warning">Host</span></p>
+                </div>
+            </div>
+        </article>
+        `;
 }
 
 chatSocket.onmessage = function(e){
     let data = JSON.parse(e.data);
-    console.log('Data', data);
+    // console.log('Data', data);
 
     if(data.type === 'response' || data.type === 'user_check')
     {
@@ -36,15 +50,21 @@ chatSocket.onmessage = function(e){
         element.innerHTML = '';
         var userHere = document.getElementById('user-here').getAttribute('value');
 
-        listUsers.forEach(val => {
+        listUsers.forEach( val => {
+
+            is_guest_val = val.slice(-1);
+            is_guest = true;
+            if(is_guest_val === "0")
+            {
+                is_guest = false;
+            }
+
+            val = val.slice(0, -1);
             if(val !== userHere)
             {
-                element.insertAdjacentHTML("beforeend", createHtmlUser(val, true));
+                element.insertAdjacentHTML("beforeend", createHtmlUser(val, is_guest));
             }
             
         });
-            
-        
     }
-
 }
